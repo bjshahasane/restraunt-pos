152e6f22852d9958a08bc8282a2cfb8594f2b76d
bhargavi-rpos <bjshahasane>
2024-11-12 17:44:52 +0530

    dropdown issue fix

--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -18,7 +18,8 @@ const SideBar = () => {
         }
     }, [])
 
-    const toggleDropdown = () => {
+    const toggleDropdown = (e) => {
+        e.preventDefault();
         setDropdownOpen(prevState => !prevState);
     };
 
@@ -55,7 +56,7 @@ const SideBar = () => {
                     </li>
                 </ul>
                 <div className="dropdown mt-auto mb-3">
-                    <a className="nav-link dropdown-toggle px-0 align-middle" href="#" id="dropdownMenuButton" onClick={toggleDropdown}>
+                    <a className="nav-link dropdown-toggle px-0 align-middle" href="#" id="dropdownMenuButton" onClick={(e)=>toggleDropdown(e)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                             <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
